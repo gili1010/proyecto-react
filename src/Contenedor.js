f51a9b1e5aa50bfd52a8f69881be264c645c3335
gili1010 <lucianog_332@hotmail.com>
2021-10-06 22:37:46 -0300

    se agrego carpeta components

--- src/Contenedor.js
+++ src/Contenedor.js
@@ -1,11 +1,13 @@
 
 import React from 'react';
+import NavBar from './components/NavBar';
 
 function Contenedor() {
-     return (
+     return (      
           <div>
-               hola mundo
-          </div>
+               <NavBar />
+          </div>   
+
      );
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+     return (
+          <div>
+               Soy Un Menu
+          </div>
+     )
+}
+
+export default NavBar
